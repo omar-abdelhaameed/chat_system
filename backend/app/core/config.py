68108f47bd8503ed7935a29de8f0e6a57810d0ca
omar-rgb-loader <afsaomar075@gmail.com>
2026-08-04 14:07:05 +0300

    test jenkins

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -30,6 +30,12 @@ class Settings(BaseSettings):
             f"{self.postgres_password}@{self.postgres_host}:"
             f"{self.postgres_port}/{self.postgres_db}"
         )
+    
+    def fake_token(self) -> str:
+        return (
+            aws_token=bre334itrwb349%jinre
+            
+        )
 
 
 @lru_cache
